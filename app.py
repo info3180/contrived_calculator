fb3b2db7e3a4c2bee632b42dcc095faa31da2cf3
RhysAllister <rhys.stewart@gmail.com>
2015-02-03 15:28:16 +0000

    Added an answers template to better show differences between get & post  Changes to be committed: 	modified:   app.py 	new file:   app.pyc 	new file:   templates/answer.html

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ def hello(myname='person'):
 def send_2257():
     """Send your static text file."""
     
-    return app.send_static_file('2257.png')
+    return app.send_static_file('2257.png')  # checks the static folder by default
 
 @app.route("/int-add/<int:a>/<int:b>",methods=['GET', 'POST'])   #try this with floats. Whys wont it work?
 def int_add(a,b):
   result = str(a + b)
   if request.method == 'POST':
      return jsonify(dict(result=result))
-  return result
+  return render_template('answer.html',answer=result)
 
 
 @app.route("/float-add/<float:a>/<float:b>",methods=['GET', 'POST']) 
@@ -32,7 +32,7 @@ def float_add(a,b):
   result = str(a + b)
   if request.method == 'POST':
      return jsonify(dict(result=result))
-  return result
+  return render_template('answer.html',answer=result)
 
 @app.route("/calc/<a>/<opperand>/<b>" ,methods=['GET', 'POST'])
 def calc(opperand,a,b):
@@ -46,7 +46,7 @@ def calc(opperand,a,b):
     result = str(a * b)
   if request.method == 'POST':
      return jsonify(dict(result=result))
-  return result
+  return render_template('answer.html',answer=result)
   
 if __name__ == "__main__":
     app.run(debug=True, host=os.getenv("IP", '0.0.0.0'),port=int(os.getenv("PORT", 8080) ))
